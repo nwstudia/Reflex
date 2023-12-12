b31ec60242359e1fd9a49501fec101df9ad40f67
nwstudia <nwstudia@gmail.com>
2023-12-12 13:30:17 +0100

    feat: added sketch of game view

--- Reflex/Views/ReflexGameView.swift
+++ Reflex/Views/ReflexGameView.swift
@@ -1,6 +1,42 @@
 //
-//  ReflexGameView.swift
-//  Reflex
+//  ThemeButton.swift
+//  MemoryGame
 //
 //  Created by student on 12/12/2023.
 //
+
+import SwiftUI
+
+struct ReflexGameView: View {
+
+    
+    private var score : some View{
+        Text("Score: 0")
+            .animation(nil)
+    }
+    private var timer : some View{
+        Text("Timer: 30")
+            .animation(nil)
+    }
+    
+    private var GameBoard : some View{
+        Text("TODO: there will be shapes genereted")
+    }
+    
+    var body: some View {
+        VStack{
+            HStack{
+                timer
+                Spacer()
+                score
+            }
+            Spacer()
+            GameBoard
+            Spacer()
+        }
+    }
+}
+
+#Preview {
+    ReflexGameView()
+}
