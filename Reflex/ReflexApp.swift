876db69d3deb44ce977a56d459da72848f558849
nwstudia <nwstudia@gmail.com>
2023-12-12 12:42:58 +0100

    Initial Commit

---
+++ Reflex/ReflexApp.swift
@@ -0,0 +1,17 @@
+//
+//  ReflexApp.swift
+//  Reflex
+//
+//  Created by student on 12/12/2023.
+//
+
+import SwiftUI
+
+@main
+struct ReflexApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
